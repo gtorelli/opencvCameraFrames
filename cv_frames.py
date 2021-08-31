473025c3f290bd2137d486a020c28b0509d5e9be
Gabriel Torelli <torelli@yahoo.com>
2021-08-31 17:50:13 -0300

    Add files via upload

---
+++ cv_frames.py
@@ -0,0 +1,29 @@
+# Run> python zoom.py 500 5
+import cv2
+import sys
+import time
+capture = cv2.VideoCapture(0) 
+count = 0
+i = 0
+
+while(True):
+     
+    ret, frame = capture.read()
+    scale_percent = int(sys.argv[1])
+    width = int(frame.shape[1] * scale_percent / 100)
+    height = int(frame.shape[0] * scale_percent / 100)
+
+    dsize = (width, height)
+    
+    frame = cv2.resize(frame, dsize) 
+    cv2.imshow('video', frame)
+    
+    cv2.imwrite('Frame'+str(i)+'.jpg', frame)
+    i += 1
+    time.sleep(int(sys.argv[2]))
+     
+    if cv2.waitKey(1) == 27:
+        break
+ 
+capture.release()
+cv2.destroyAllWindows()
